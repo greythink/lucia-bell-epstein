d82804afabb3b5c5d226c95eb6cd5e188eb2e8ff
Archit Chandra <architchandra@gmail.com>
2024-09-27 19:55:55 +0530

    Set up fonts and base colours

--- src/assets/tailwind.config.js
+++ src/assets/tailwind.config.js
@@ -2,12 +2,25 @@
 
 
 
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+
+
 module.exports = {
     content: [
         './src/**/*.njk',
     ],
     safelist: [],
     theme: {
+        colors: {
+            transparent: 'transparent',
+            current: 'currentColor',
+            black: '#000000',
+        },
+        fontFamily: {
+            body: ['Courier Prime'].concat(defaultTheme.fontFamily.mono),
+        },
+
         extend: {},
     },
     plugins: [],
