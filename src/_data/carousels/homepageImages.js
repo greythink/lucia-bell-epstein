b099dd82ece1558d6d6e6e27e3f35f080d59ed88
Archit Chandra <architchandra@gmail.com>
2024-10-01 00:43:48 +0530

    Add dummy images, adjust carousel image sizing

--- src/_data/carousels/homepageImages.js
+++ src/_data/carousels/homepageImages.js
@@ -1,4 +1,46 @@
 module.exports = [{
-    url: '/media/info/scribbles.png',
+    url: '/media/home/lbe_carousel_1.jpg',
+    alt: 'Test tsttsg',
+},{
+    url: '/media/home/lbe_carousel_2.jpg',
+    alt: 'Test tsttsg',
+},{
+    url: '/media/home/lbe_carousel_3.jpg',
+    alt: 'Test tsttsg',
+},{
+    url: '/media/home/lbe_carousel_4.jpg',
+    alt: 'Test tsttsg',
+},{
+    url: '/media/home/lbe_carousel_5.jpg',
+    alt: 'Test tsttsg',
+},{
+    url: '/media/home/lbe_carousel_6.jpg',
+    alt: 'Test tsttsg',
+},{
+    url: '/media/home/lbe_carousel_7.jpg',
+    alt: 'Test tsttsg',
+},{
+    url: '/media/home/lbe_carousel_8.jpg',
+    alt: 'Test tsttsg',
+},{
+    url: '/media/home/lbe_carousel_9.jpg',
+    alt: 'Test tsttsg',
+},{
+    url: '/media/home/lbe_carousel_10.jpg',
+    alt: 'Test tsttsg',
+},{
+    url: '/media/home/lbe_carousel_11.jpg',
+    alt: 'Test tsttsg',
+},{
+    url: '/media/home/lbe_carousel_12.jpg',
+    alt: 'Test tsttsg',
+},{
+    url: '/media/home/lbe_carousel_13.jpg',
+    alt: 'Test tsttsg',
+},{
+    url: '/media/home/lbe_carousel_14.jpg',
+    alt: 'Test tsttsg',
+},{
+    url: '/media/home/lbe_carousel_15.jpg',
     alt: 'Test tsttsg',
 }];
