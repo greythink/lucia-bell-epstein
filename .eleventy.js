9864b6fde3879b603ccedca876ba3395a6d79ae7
Archit Chandra <architchandra@gmail.com>
2024-09-30 21:50:33 +0530

    Install Splide slider

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,11 @@ module.exports = function (config) {
     // Set output paths
     config.addPassthroughCopy('src/media');
 
+    // Copy library files
+    config.addPassthroughCopy({
+        'node_modules/@splidejs/splide/dist/js/splide.min.js': 'assets/js/libs/splide.min.js'
+    });
+
     return {
       dir: {
         input: 'src',
