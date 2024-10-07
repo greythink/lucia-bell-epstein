969d1c137915ce09399f08e9a86cac7b57f4105a
Archit Chandra <architchandra@gmail.com>
2024-10-07 15:46:03 +0530

    Undo temporary workaround, calculate CSS variables on first load

--- build/assets/js/urgent.js
+++ build/assets/js/urgent.js
@@ -1,6 +1,8 @@
 // Calculate JS-assisted values
-window.addEventListener('resize', function () {
+function calculateValues () {
     var vh = window.innerHeight * 0.01;
     // Create --vh CSS variable
     document.documentElement.style.setProperty('--vh', `${vh}px`);
-});
+}
+calculateValues();
+window.addEventListener('resize', calculateValues);
