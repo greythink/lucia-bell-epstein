20403bd91d2e485eac44b9b47efb1f3096ee154d
Archit Chandra <architchandra@gmail.com>
2024-10-08 19:11:19 +0530

    Update alt text for homepage carousel images

--- src/_data/carousels/homepageImages.js
+++ src/_data/carousels/homepageImages.js
@@ -1,46 +1,46 @@
 module.exports = [{
     url: '/media/home/lbe_carousel_1.jpg',
-    alt: 'Test tsttsg',
+    alt: 'Still life of amaretti cookies and coffee on blue',
 },{
     url: '/media/home/lbe_carousel_2.jpg',
-    alt: 'Test tsttsg',
+    alt: 'Four girls in back of a car squished together',
 },{
     url: '/media/home/lbe_carousel_3.jpg',
-    alt: 'Test tsttsg',
+    alt: 'Man with arms sprawled over table',
 },{
     url: '/media/home/lbe_carousel_4.jpg',
-    alt: 'Test tsttsg',
+    alt: 'Pots and pans in restaurant kitchen',
 },{
     url: '/media/home/lbe_carousel_5.jpg',
-    alt: 'Test tsttsg',
+    alt: 'Sausage plate served at a bar',
 },{
     url: '/media/home/lbe_carousel_6.jpg',
-    alt: 'Test tsttsg',
+    alt: 'Woman in a green field laying on a haystack, arms opened',
 },{
     url: '/media/home/lbe_carousel_7.jpg',
-    alt: 'Test tsttsg',
+    alt: 'Salting a fish',
 },{
     url: '/media/home/lbe_carousel_8.jpg',
-    alt: 'Test tsttsg',
+    alt: 'Box of pink radicchio and green leeks',
 },{
     url: '/media/home/lbe_carousel_9.jpg',
-    alt: 'Test tsttsg',
+    alt: 'Man turned away, wearing all white, hands clasped behind his back with palms open',
 },{
     url: '/media/home/lbe_carousel_10.jpg',
-    alt: 'Test tsttsg',
+    alt: 'Single cocktail on red surface next to black velvet curtains',
 },{
     url: '/media/home/lbe_carousel_11.jpg',
-    alt: 'Test tsttsg',
+    alt: 'Three chefs huddled together in a restaurant kitchen',
 },{
     url: '/media/home/lbe_carousel_12.jpg',
-    alt: 'Test tsttsg',
+    alt: 'Produce tray with all green ingredients, organized by kind: limes, cucumbers, melons, and chives',
 },{
     url: '/media/home/lbe_carousel_13.jpg',
-    alt: 'Test tsttsg',
+    alt: 'Interior detail of Paris restaurant Clown Bar',
 },{
     url: '/media/home/lbe_carousel_14.jpg',
-    alt: 'Test tsttsg',
+    alt: 'Man shaving in the mirror',
 },{
     url: '/media/home/lbe_carousel_15.jpg',
-    alt: 'Test tsttsg',
+    alt: 'Green artichoke served with glass of beer ',
 }];
